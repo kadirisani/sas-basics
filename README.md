600df42e27a087ce6b93b7205c2039951a6c1578
Ramesh Kadirisani <kadirisani@Rameshs-Air.home>
2017-01-16 16:54:04 -0500

    adds all other pages

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # sas-basics
 sas basics and survival kit
 
-# How to deploy heroku app
+## How to deploy heroku app
 
 [How to deploy a NodeJS app guidelines are given in detail here](https://devcenter.heroku.com/articles/getting-started-with-nodejs#introduction)
 
+## Useful links
 
-[And this is the link Markdown syntax cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links)
+[And this is the link to Markdown syntax cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links)
+
+## Other info
+
+To destory an existing app from heroku, use the following command:
+
+```
+do heroku apps:destroy --app $app --confirm $app;
+```
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+
+router.use(function(request, response, next){
+  console.log("/" + request.method);
+  next();
+});
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,8 +15,22 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 
-app.get('/', function(request, response){
+router.get('/', function(request, response){
 	response.sendFile(__dirname + '/views/pages/index.html');
+});
+
+router.get('/about', function(request, response){
+  response.sendFile(__dirname + '/views/pages/about.html');
+});
+
+router.get('/contact', function(request, response){
+  response.sendFile(__dirname + '/views/pages/contact.html');
+});
+
+app.use('/', router);
+
+app.use('*', function(request,response){
+  response.sendFile(__dirname + '/public/error.html')
 });
 
 app.listen(app.get('port'), function() {
